ed4cb5f05b2938e8e8128108dd3eb3ceb1a84392
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2025-05-07 00:30:30 +0200

    adding favicon.ico

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,12 @@ import "./globals.css";
 import Navbar from "@/components/utils/Navbar";
 import Footer from "@/components/layout/Footer";
 
+export const metadata = {
+  title: 'Firen Antincendio',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -14,9 +20,13 @@ export default function RootLayout({ children }) {
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       </head>
       <body className="flex flex-col min-h-screen">
-        <main className="flex-grow pt-30"> {/* pt-20 */}
+        <main className="flex-grow pt-30">
           {children}
         </main>
         <Navbar />
